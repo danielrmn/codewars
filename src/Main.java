e45cd459a1fb62892c08d6881589512485e161a1
daniel <daniel.dl14121@gmail.com>
2022-10-28 16:22:40 -0600

    initial uppercase string algorithm 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String variable = String.valueOf(toCamelCase("hola_mundo_soy_programdor_java"));
+    }
+
+    static String toCamelCase(String s){
+        char[] caracteres = s.toCharArray();
+        caracteres[0] = Character.toUpperCase(caracteres[0]);
+        for (int i = 0; i < s.length()- 2; i++)
+            // Es 'palabra'
+            if (caracteres[i] == ' ' || caracteres[i] == '.' || caracteres[i] == ','|| caracteres[i] == '_')
+                // Reemplazamos
+                caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
+        System.out.println(caracteres);
+
+        return "";
     }
 }
